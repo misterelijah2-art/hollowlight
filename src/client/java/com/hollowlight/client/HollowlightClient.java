8eacd907d9ab2297036f099837ca9d9c0058d1b0
misterelijah2-art <misterelijah2@gmail.com>
2026-09-12 19:55:23 +0200

    Register Stalker EntityModelLayer to prevent runtime crash

--- src/client/java/com/hollowlight/client/HollowlightClient.java
+++ src/client/java/com/hollowlight/client/HollowlightClient.java
@@ -4,11 +4,13 @@ import com.hollowlight.client.render.StalkerEntityRenderer;
 import com.hollowlight.client.render.WatcherEntityRenderer;
 import com.hollowlight.entity.ModEntities;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 
 /**
- * Client-side entrypoint. Registers entity renderers and the dread HUD
+ * Client-side entrypoint. Registers entity renderers, the model layer the
+ * Stalker's placeholder QuadrupedEntityModel needs, and the dread HUD
  * overlay. Screen-space post-processing (vignette at high dread) is
  * implemented via HUD render layers in {@link com.hollowlight.client.gui.DreadHud};
  * a true shader-based post-process pipeline is left as a roadmap TODO
@@ -18,6 +20,8 @@ public final class HollowlightClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+		EntityModelLayerRegistry.registerModelLayer(StalkerEntityRenderer.MODEL_LAYER, StalkerEntityRenderer::getTexturedModelData);
+
 		EntityRendererRegistry.register(ModEntities.WATCHER, WatcherEntityRenderer::new);
 		EntityRendererRegistry.register(ModEntities.STALKER, StalkerEntityRenderer::new);
 
