1c5074e625d749ae083b08ac60ce55dd3daea161
misterelijah2-art <misterelijah2@gmail.com>
2026-09-12 19:26:16 +0200

    Fix ModLootTableProvider to use SimpleFabricLootTableProvider API

--- src/main/java/com/hollowlight/datagen/ModLootTableProvider.java
+++ src/main/java/com/hollowlight/datagen/ModLootTableProvider.java
@@ -1,39 +1,53 @@
 package com.hollowlight.datagen;
 
+import com.hollowlight.Hollowlight;
 import com.hollowlight.entity.ModEntities;
 import com.hollowlight.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricEntityLootTableProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
 /**
  * Entity loot tables: The Watcher has a rare (1/20) Starshard Fragment drop
  * on death, reinforcing that killing it (hard, since it barely moves but
  * blink-punishes aggression) is a valid alternate route to artifact
  * ingredients versus finding Chapel ritual structures.
+ *
+ * Uses SimpleFabricLootTableProvider with LootContextTypes.ENTITY, the
+ * correct base class for entity loot in this API version — there is no
+ * "FabricEntityLootTableProvider" class; that name does not exist in the
+ * Fabric API for 1.21.1.
  */
-public class ModLootTableProvider extends FabricEntityLootTableProvider {
+public class ModLootTableProvider extends SimpleFabricLootTableProvider {
 
-	protected ModLootTableProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-		super(dataOutput, registriesFuture);
+	public static final RegistryKey<LootTable> WATCHER_LOOT = RegistryKey.of(RegistryKeys.LOOT_TABLE, Identifier.of(Hollowlight.MOD_ID, "entities/watcher"));
+	public static final RegistryKey<LootTable> STALKER_LOOT = RegistryKey.of(RegistryKeys.LOOT_TABLE, Identifier.of(Hollowlight.MOD_ID, "entities/stalker"));
+
+	public ModLootTableProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+		super(dataOutput, registriesFuture, LootContextTypes.ENTITY);
 	}
 
 	@Override
-	public void generate() {
-		register(ModEntities.WATCHER, LootTable.builder()
+	public void generate(BiConsumer<RegistryKey<LootTable>, LootTable.Builder> lootTableBiConsumer) {
+		lootTableBiConsumer.accept(WATCHER_LOOT, LootTable.builder()
 				.pool(LootPool.builder()
 						.rolls(ConstantLootNumberProvider.create(1))
 						.with(ItemEntry.builder(ModItems.STARSHARD_FRAGMENT).build())
 						.conditionally(RandomChanceLootCondition.builder(0.05f))));
 
-		register(ModEntities.STALKER, LootTable.builder()
+		lootTableBiConsumer.accept(STALKER_LOOT, LootTable.builder()
 				.pool(LootPool.builder()
 						.rolls(ConstantLootNumberProvider.create(1))
 						.with(ItemEntry.builder(net.minecraft.item.Items.BONE).build())));
